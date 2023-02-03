f9aec013e067c2b061a442c977a79f0cbed809cc
bamboo_octopus <bamboooctopus@gmail.com>
2023-02-03 17:12:38 -0500

    changed index.html && webpack.config to 'bundle.js'

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+console.log("hello world");
+console.log("second hello");
+
+
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+
+  mode: 'none',
+
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+    
   },
 };
